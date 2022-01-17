0dea5be635260ed7925f46607a850159f01da123
Matthew Avis <mla531@mun.ca>
2022-01-17 14:24:23 -0330

    Demo project for Outline and SuitabilityPrediction (#1)

---
+++ examples/iOS/LibraryExamples/SuitabilityPredictionExample/SuitabilityPredictionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuitabilityPredictionExampleApp.swift
+//  SuitabilityPredictionExample
+//
+//  Created by Matthew Avis on 2022-01-14.
+//
+
+import SwiftUI
+
+@main
+struct SuitabilityPredictionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
